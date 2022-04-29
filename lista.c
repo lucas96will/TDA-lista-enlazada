7adc47bcf8b88709324538412366e71e851039ad
pedrogrin <91101862+pedrogrin@users.noreply.github.com>
2022-04-28 19:27:52 -0700

    Add files via upload

--- lista.c
+++ lista.c
@@ -1,7 +1,10 @@
 #include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /* Definición del struct nodo*/
-typedef stuct nodo{
+typedef struct nodo{
         void* dato;
         struct nodo* siguiente;
 } nodo_t;
@@ -12,3 +15,168 @@ struct lista{
     nodo_t* ultimo;
     size_t largo;
 };
+
+/* Definición del struct lista_iter*/
+struct lista_iter {
+	lista_t* lista;
+	struct nodo* act;
+	struct nodo* ant;
+};
+
+/* ******************************************************************
+ *                FUNCIONES AUXILIARES PARA NODOS
+ * *****************************************************************/
+
+nodo_t* crear_nodo(void);
+void destruir_nodo(nodo_t* nodo);
+
+nodo_t* crear_nodo(void) {
+    nodo_t* nodo= malloc(sizeof(nodo_t));
+    if (nodo == NULL) {
+        return NULL;
+    }
+    return nodo;
+}
+
+void destruir_nodo(nodo_t* nodo) {
+    free(nodo);
+}
+
+/* *****************************************************************
+ *                    PRIMITIVAS DE LA LISTA
+ * *****************************************************************/
+
+lista_t *lista_crear(void) {
+    lista_t* lista = malloc(sizeof(lista_t));
+    if (lista == NULL) {
+        return lista;
+    }
+
+    lista->primero = NULL;
+    lista->ultimo = NULL;
+    lista->largo = 0;
+    return lista;
+}
+
+bool lista_esta_vacia(const lista_t *lista) {
+    if (lista->largo == 0 && lista->primero == NULL && lista->ultimo == NULL) {
+        return true;
+    }
+    return false;
+}
+
+bool lista_insertar_primero(lista_t *lista, void *dato) {
+    nodo_t* nodo = crear_nodo();
+    if (nodo == NULL) {
+        return false;
+    }
+
+    nodo->dato = dato;
+
+    if (lista->primero == NULL && lista->ultimo == NULL) {
+        lista->primero = nodo;
+        lista->ultimo  = nodo;
+    } else {
+        nodo_t* aux = lista->primero;
+        lista->primero = nodo;
+        nodo->siguiente = aux;
+    }
+
+    lista->largo++;
+    return true;
+
+}
+
+bool lista_insertar_ultimo(lista_t *lista, void *dato) {
+    nodo_t* nodo = crear_nodo();
+    if (nodo == NULL) {
+        return false;
+    }
+
+    nodo->dato = dato;
+
+    if (lista->primero == NULL && lista->ultimo == NULL) {
+        lista->primero = nodo;
+        lista->ultimo  = nodo;
+    } else {
+        lista->ultimo->siguiente = nodo;
+        lista->ultimo = nodo;
+    }
+
+    nodo->siguiente = NULL;
+    lista->largo++;
+    return true;
+}
+
+void *lista_borrar_primero(lista_t *lista) {
+    if (lista_esta_vacia(lista)) {
+        return NULL;
+    }
+    nodo_t* primer_nodo = lista->primero;
+    void* valor = primer_nodo->dato;
+
+    if (primer_nodo->siguiente == NULL) {
+        lista->primero = NULL;
+        lista->ultimo = NULL;
+    } else {
+        lista->primero = primer_nodo->siguiente;
+    }
+
+    lista->largo--;
+    destruir_nodo(primer_nodo);
+    return valor;
+}
+
+void *lista_ver_primero(const lista_t *lista) {
+    if (lista_esta_vacia(lista)) {
+        return NULL;
+    }
+    return lista->primero->dato;
+}
+
+void *lista_ver_ultimo(const lista_t* lista) {
+    if (lista_esta_vacia(lista)) {
+        return NULL;
+    }
+    return lista->ultimo->dato;
+}
+
+size_t lista_largo(const lista_t *lista) {
+    size_t largo = lista->largo;
+    return largo;
+}
+
+void lista_destruir(lista_t *lista, void (*destruir_dato)(void *)) {
+    while (!lista_esta_vacia(lista)) {
+        void* elemento_eliminado = lista_borrar_primero(lista);
+         if (destruir_dato != NULL){
+            destruir_dato(elemento_eliminado);
+        }
+    }
+    free(lista);    
+    return;
+}
+
+/* *****************************************************************
+ *               PRIMITIVA DEL ITERADOR INTERNO
+ * *****************************************************************/
+
+void lista_iterar(lista_t *lista, bool visitar(void *dato, void *extra), void *extra);
+
+/* *****************************************************************
+ *               PRIMITIVAS DEL ITERADOR EXTERNO
+ * *****************************************************************/
+
+lista_iter_t *lista_iter_crear(lista_t *lista);
+
+bool lista_iter_avanzar(lista_iter_t *iter);
+
+void *lista_iter_ver_actual(const lista_iter_t *iter);
+
+bool lista_iter_al_final(const lista_iter_t *iter);
+
+void lista_iter_destruir(lista_iter_t *iter);
+
+bool lista_iter_insertar(lista_iter_t *iter, void *dato);
+
+void *lista_iter_borrar(lista_iter_t *iter);
--- lista.h
+++ lista.h
@@ -1,27 +1,97 @@
 #ifndef TDA_LISTA_ENLAZADA_LISTA_H
 #define TDA_LISTA_ENLAZADA_LISTA_H
 
+#include <stdbool.h>
+
+
 /* *****************************************************************
  *                DEFINICION DE LOS TIPOS DE DATOS
  * *****************************************************************/
 
 /* Breve descripción */
 
+struct lista;
 typedef struct lista lista_t;
 
+struct lista_iter;
+typedef struct lista_iter lista_iter_t;
 
 /* *****************************************************************
- *                    PRIMITIVAS DE LA PILA
+ *                    PRIMITIVAS DE LA LISTA
  * *****************************************************************/
 
+// Crea una lista.
+// Post: se devolvió una nueva lista vacia.
+lista_t *lista_crear(void);
 
+// Devuelve true si la lista no tiene elementos, false en caso contrario.
+// Pre: la lista fue creada.
+bool lista_esta_vacia(const lista_t *lista);
 
+// Inserta un elemento que pasa a ser el primero de la lista. Devuelve false en caso de error.
+// Pre: la lista fue creada.
+// Post: se insertó un nuevo elemento a la lista, que se encuentra al inicio de la misma.
+bool lista_insertar_primero(lista_t *lista, void *dato);
 
+// Inserta un elemento que pasa a ser el ultimo de la lista. Devuelve false en caso de error.
+// Pre: la lista fue creada.
+// Post: se insertó un nuevo elemento a la lista, que se encuentra al final de la misma.
+bool lista_insertar_ultimo(lista_t *lista, void *dato);
 
+// Elimina el primer elemento de la lista. Si la lista tiene elementos, se quita el
+// primero de la lista, y se devuelve su valor, si está vacía, devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el valor del primer elemento anterior(el eliminado), la lista
+// contiene un elemento menos, si la lista no estaba vacía.
+void *lista_borrar_primero(lista_t *lista);
 
+// Obtiene el valor del primer elemento de la lista. Si la lista tiene
+// elementos, se devuelve el valor del primero, si está vacía devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el primer elemento de la lista, cuando no está vacía.
+void *lista_ver_primero(const lista_t *lista);
 
+// Obtiene el valor del ultimo elemento de la lista. Si la lista tiene
+// elementos, se devuelve el valor del ultimo, si está vacía devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el ultimo elemento de la lista, cuando no está vacía.
+void *lista_ver_ultimo(const lista_t* lista);
 
+// Devuelve el largo de la lista, cero si esta vacia.
+// Pre: la lista fue creada.
+// Post: se devolvió el largo de la lista.
+size_t lista_largo(const lista_t *lista);
 
+// Destruye la lista. Si se recibe la función destruir_dato por parámetro,
+// para cada uno de los elementos de la lista llama a destruir_dato.
+// Pre: la lista fue creada. destruir_dato es una función capaz de destruir
+// los datos de la lista, o NULL en caso de que no se la utilice.
+// Post: se eliminaron todos los elementos de la lista.
+void lista_destruir(lista_t *lista, void (*destruir_dato)(void *));
+
+/* *****************************************************************
+ *               PRIMITIVA DEL ITERADOR INTERNO
+ * *****************************************************************/
+
+void lista_iterar(lista_t *lista, bool visitar(void *dato, void *extra), void *extra);
+
+/* *****************************************************************
+ *               PRIMITIVAS DEL ITERADOR EXTERNO
+ * *****************************************************************/
+
+lista_iter_t *lista_iter_crear(lista_t *lista);
+
+bool lista_iter_avanzar(lista_iter_t *iter);
+
+void *lista_iter_ver_actual(const lista_iter_t *iter);
+
+bool lista_iter_al_final(const lista_iter_t *iter);
+
+void lista_iter_destruir(lista_iter_t *iter);
+
+bool lista_iter_insertar(lista_iter_t *iter, void *dato);
+
+void *lista_iter_borrar(lista_iter_t *iter);
 
 /* *****************************************************************
  *                    PRUEBAS PARA LA LISTA
@@ -29,7 +99,6 @@ typedef struct lista lista_t;
 
 // Realiza pruebas sobre la implementación realizada.
 void pruebas_lista_estudiante(void);
-
 
 
 #endif //TDA_LISTA_ENLAZADA_LISTA_H
